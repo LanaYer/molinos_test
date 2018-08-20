cdf28bd630f0ffd55417dc0abd197d228cccbc14
LanaYer <yermolaeva.lana@gmail.com>
2018-08-20 21:50:27 +0300

    link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,7 @@
                             <li><a href="{{ route('login') }}">Вход</a></li>
                             <li><a href="{{ route('register') }}">Регистрация</a></li>
                         @else
+                            <li><a href="{{ route('home') }}">Список заявок</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
